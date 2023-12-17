70d3c1a951909376718a74c162d1fe2fb935e6a6
divinejoshua <divineuche399@gmail.com>
2023-12-17 21:23:59 +0000

    Removed default styling and added header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className='pb-10'>
+        <Link href={'/'}>
+            Hotseat
+        </Link>
+    </header>
+  )
+}
