53d7087e62f99782c3d7f4fd96b58067c4f3a6c7
divinejoshua <divineuche399@gmail.com>
2023-12-17 22:17:26 +0000

    Added action buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,18 @@ export default function Home() {
           className="eighteen-image mt-1"
         />
       </p>
+
+      {/* Action buttons  */}
+      <center>
+        <section className="max-w-max">
+          <button className='btn flex py-4 place-content-center mt-7 bg-blue-500 text-white w-full px-14 rounded-full font-bold drop-shadow'>
+            Start new game
+          </button>
+          <button className='btn flex py-4 place-content-center mt-7 bg-indigo-400 text-white w-full px-14 rounded-full font-bold drop-shadow'>
+            Join game
+          </button>
+        </section>
+      </center>
     </main>
   )
 }
