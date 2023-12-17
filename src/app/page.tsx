0f564d86810fe679d789c93ab3660988517bb770
divinejoshua <divineuche399@gmail.com>
2023-12-17 22:01:38 +0000

    Added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,26 @@
 import Header from "./components/Header";
+import emojiImage from "./assets/emoji.png"
+import eighteenImage from "./assets/eighteenplus.png"
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="">
-        <Header/>
-      <p className='text-3xl'>Text</p>
+    <main className="text-center">
+      <Header/>
+      <p className='mt-12 text-2xl flex justify-center'>
+        <Image
+          src={emojiImage}
+          alt="Hotseat"
+          className="emoji-image"
+        />
+        &nbsp;Welcome to Hot seat game&nbsp;
+        <Image
+          src={eighteenImage}
+          alt="Hotseat"
+          width={20}
+          className="eighteen-image mt-1"
+        />
+      </p>
     </main>
   )
 }
