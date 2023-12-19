74ba0ece90b2e66d7e35cd006e4c12afad7b7f3b
divinejoshua <divineuche399@gmail.com>
2023-12-19 21:43:27 +0000

    Updated strict mode to false

--- src/app/(pages)/join/page.tsx
+++ src/app/(pages)/join/page.tsx
@@ -3,7 +3,7 @@ import JoinGameForm from "@/app/components/JoinGameForm";
 
 export default function JoinGamePage() {
   return (
-    <main className="">
+    <main className="px-4">
         <Header/>
         <p className="mt-3 text-lg mb-3">Join a new game</p>
         <JoinGameForm/>
