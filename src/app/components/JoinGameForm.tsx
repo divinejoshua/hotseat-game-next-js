760c8d43a17fd9801ba04aa32fd06cd8f3f8a5a4
divinejoshua <divineuche399@gmail.com>
2023-12-19 00:21:17 +0000

    Added design for join game form

--- src/app/components/JoinGameForm.tsx
+++ src/app/components/JoinGameForm.tsx
@@ -1,7 +1,18 @@
 'use client'
+import { useState } from "react"
 
 export default function JoinGameForm() {
+  const [count, setcount] = useState<number>(0)
   return (
-    <div>JoinForm</div>
+    <div>
+     <input
+      type="number"
+      placeholder="Enter game code"
+      className="form-control text-center text-lg mt-1 w-full py-3 px-3 tracking-widest rounded border border-gray-300 focus:outline-none focus:border-blue-500 focus:ring-blue-500 focus:ring-1 focus:border-100 transition duration-0 hover:duration-150"
+     />
+      <button className='btn flex py-3 px-10 place-content-center mt-7 bg-blue-500 text-white rounded-lg w-full font-bold drop-shadow'>
+        Join game
+      </button>
+    </div>
   )
 }
