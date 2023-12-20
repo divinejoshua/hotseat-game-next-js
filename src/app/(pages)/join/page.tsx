749c5cf00ab8256a48605db4161845ed22523a67
divinejoshua <divineuche399@gmail.com>
2023-12-20 23:12:09 +0000

    Updated file structure

--- src/app/(pages)/join/page.tsx
+++ src/app/(pages)/join/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/app/components/Header";
-import JoinGameForm from "@/app/components/JoinGameForm";
+import JoinGameForm from "@/app/(pages)/join/JoinGameForm";
 
 export default function JoinGamePage() {
   return (
