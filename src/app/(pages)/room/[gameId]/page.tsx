b0744e0b19e806a434feb1498812a12042a03fff
divinejoshua <divineuche399@gmail.com>
2023-12-24 18:49:15 +0000

    Display player list successfully

--- src/app/(pages)/room/[gameId]/page.tsx
+++ src/app/(pages)/room/[gameId]/page.tsx
@@ -1,9 +1,61 @@
+"use client"
 import Header from "@/app/components/Header"
 import PlayerList from "@/app/components/PlayerList"
+import firebase from "@/app/utils/firebase";
 import Link from "next/link"
-
+import {
+  doc,
+  onSnapshot,
+  setDoc,
+  collection,
+  query,
+  where,
+  orderBy,
+} from 'firebase/firestore';
+import { useEffect, useState } from "react";
 export default function GamePage({ params } : any) {
+
+  // DATA
   const gameId = params.gameId
+  const playersColletionRef = collection(firebase, 'players');
+  const [playerList, setplayerList] = useState<[]>([])
+  const [playerListOrder, setplayerListOrder] = useState<[]>([])
+
+
+  // Re order playerList by time created
+  const reorderPlayerListAccendingOrder = (playerList : any) =>{
+    playerList.sort((a : any , b : any) => a.createdAt - b.createdAt);
+    setplayerListOrder(playerList)
+  }
+
+  useEffect(() => {
+    // Query Statement
+    const queryClause = query(
+      playersColletionRef,
+      where('game_id', '==', gameId),
+    );
+
+    // Get messages from database
+    const getPlayerList = onSnapshot(queryClause, (querySnapshot) => {
+      const response : [] | any = [];
+      querySnapshot.forEach((doc) => {
+        response.push(doc.data())
+      });
+      setplayerList(response)
+    })
+
+    return () => {
+      getPlayerList
+    }
+  }, [])
+
+    // Player list
+    useEffect(() => {
+      reorderPlayerListAccendingOrder(playerList)
+      // Call the function and log the reordered list
+      return () => {
+      }
+    }, [playerList])
 
   return (
     <main className="">
@@ -14,7 +66,7 @@ export default function GamePage({ params } : any) {
       </div>
 
       {/* Player list */}
-      <PlayerList/>
+      <PlayerList playerList={playerListOrder}/>
 
       {/* This button is only available to the game admin */}
       {/* The route is /game/<gameID>/<gameRoundId> */}
--- src/app/components/PlayerList.tsx
+++ src/app/components/PlayerList.tsx
@@ -1,37 +1,26 @@
-export default function PlayerList() {
-  return (
-    <div className="px-2">
-        {/* Active players list  */}
-        <p className="text-lg mt-5"><span className="text-blue-500 font-bold">13</span> player(s) online</p>
+export default function PlayerList(props : any) {
+    let playerList = props.playerList
+    return (
+        <div className="px-2">
+            {/* Active players list  */}
+            <p className="text-lg mt-5"><span className="text-blue-500 font-bold">13</span> player(s) online</p>
 
-        <div className="grid grid-cols-2 gap-4 mt-5">
+            <div className="grid grid-cols-2 gap-4 mt-5">
 
-            <div className="row-span-2 col-span-1 border rounded-lg shadow-sm px-3 py-3">
-                <div className="player-card flex">
-                    <p className="truncate">{"Player 1"}</p>
-                    <span className="ml-4 bg-pink-100 text-xs text-pink-500 drop-shadow-xs rounded-lg px-3 py-1">Host</span>
-                </div>
+                {/* Player list */}
+                {
+                Array.isArray(playerList) ? playerList.map((player, index) => (
+                    <div className="row-span-2 col-span-1 border rounded-lg shadow-sm px-3 py-3" key={index}>
+                        <div className="player-card flex">
+                            <p className="truncate">{player.player_name}</p>
+
+                            {/* Display this for the game admin */}
+                            { index === 0 && <span className="ml-4 bg-pink-100 text-xs text-pink-500 drop-shadow-xs rounded-lg px-3 py-1">Host</span>}
+                        </div>
+                    </div>
+                )) : ''
+                }
             </div>
-
-            <div className="row-span-2 col-span-1 border rounded-lg shadow-sm px-3 py-3">
-                <div className="player-card flex">
-                    <p className="truncate">{"Player 2"}</p>
-                </div>
-            </div>
-
-            <div className="row-span-2 col-span-1 border rounded-lg shadow-sm px-3 py-3">
-                <div className="player-card flex">
-                    <p className="truncate">{"Player 3"}</p>
-                </div>
-            </div>
-
-            <div className="row-span-2 col-span-1 border rounded-lg shadow-sm px-3 py-3">
-                <div className="player-card flex">
-                    <p className="truncate">{"Player 4"}</p>
-                </div>
-            </div>
-
         </div>
-    </div>
-  )
+    )
 }
