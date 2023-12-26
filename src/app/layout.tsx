988fea918aa4f1a07bb648e26eec473281d19a30
divinejoshua <divineuche399@gmail.com>
2023-12-26 23:50:53 +0000

    Added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Baloo_2 } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 
 const inter = Baloo_2({ subsets: ['latin'] })
 
@@ -21,6 +22,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+
+      {/* Google tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-WSGW0XJGWD"
+        strategy="afterInteractive"
+        async
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-WSGW0XJGWD');
+        `}
+      </Script>
+
       <body className={inter.className}>{children}</body>
     </html>
   )
